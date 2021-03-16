e1215a0877c06275220d519879998939521e084a
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2021-03-16 14:43:56 +0000

    Replace strdup(). Fixes BZ-6103

--- src/molpro/PluginGuest.cpp
+++ src/molpro/PluginGuest.cpp
@@ -98,5 +98,10 @@ static std::shared_ptr<molpro::PluginGuest> guest = nullptr;
 void PluginGuestOpen(const char* host) { guest = std::make_shared<molpro::PluginGuest>(std::string(host)); }
 int PluginGuestActive() { return guest!=nullptr && guest->active() ? 1 : 0;}
 int PluginGuestSend(const char* value) { return guest->send(std::string(value)) ? 1 : 0 ; }
-const char* PluginGuestReceive() { return strdup(guest->receive().c_str()); }
+const char *PluginGuestReceive() {
+  auto message_string = guest->receive();
+  char *message = (char *) malloc(message_string.size() + 1);
+  strcpy(message, message_string.c_str());
+  return message;
+}
 void PluginGuestClose() { guest->close(); }
